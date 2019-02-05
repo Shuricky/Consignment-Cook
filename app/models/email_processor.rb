901197a48daab11deb5717fc2b907527dfa3e522
Shurick <alexr0909@yahoo.com>
2019-02-04 22:24:28 -0500

    7 does the trick

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,9 +1,10 @@
 class EmailProcessor
   def initialize(email)
+    puts 'give me something'
     @email = email
   end
 
   def process
-    Shoe.create!({sku: 'EmailParse', size: 10, quantity: 3, price: 100, user_id: 1})
+    Shoe.create!({sku: 'EmailParse', size: '10', quantity: 3, price: 100, user_id: 1})
   end
 end
