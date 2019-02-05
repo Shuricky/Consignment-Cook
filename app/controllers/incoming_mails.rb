fa274a16a3c841095c052c6a42dadb97cab2dcf6
Shurick <alexr0909@yahoo.com>
2019-02-05 05:07:52 -0500

    15

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -1,15 +1,9 @@
 class IncomingMailsController < ApplicationController
-
+  protect_from_forgery with: :null_session
 
   def create
-    Rails.logger.log params[:envelope][:to] # print the to field to the logs
-    Rails.logger.log params[:subject] # print the subject to the logs
-    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
-    Rails.logger.log params[:html] # print the html decoded body to the logs if present
-    Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
-
-    # Do some other stuff with the mail message
-
-    render :text => 'success', :status => 200 # a status of 404 would reject the mail
+    puts 'typethisinformationouthereadfhsdf'
+    Rails.logger.debug params.inspect
+    Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
   end
 end
