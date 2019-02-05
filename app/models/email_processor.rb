88c5e705cd0478d82cafc22f5745502e983d173a
Shurick <alexr0909@yahoo.com>
2019-02-04 22:19:52 -0500

    not really sure 6 I think

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -1,5 +1,9 @@
 class EmailProcessor
-  def self.process(email)
+  def initialize(email)
+    @email = email
+  end
+
+  def process
     Shoe.create!({sku: 'EmailParse', size: 10, quantity: 3, price: 100, user_id: 1})
   end
 end
