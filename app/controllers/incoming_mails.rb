4a563aa054353526af61fcb8d82b64980eadcbe2
Shurick <alexr0909@yahoo.com>
2019-02-06 02:49:57 -0500

    who even knows 47

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -6,5 +6,14 @@ class IncomingMailsController < ApplicationController
     Rails.logger.debug params.inspect
     Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
     Rails.logger.debug params[:plain]
+
+    style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(\d+)/).flatten
+
+    Rails.logger.debug style
+    Rails.logger.debug price
+    Rails.logger.debug stock
+
+
+
   end
 end
