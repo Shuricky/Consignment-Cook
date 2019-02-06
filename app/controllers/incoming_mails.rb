704459a2c98aa59a78d633233f2dc25a0135aa74
Shurick <alexr0909@yahoo.com>
2019-02-06 02:24:14 -0500

    46

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -3,11 +3,8 @@ class IncomingMailsController < ApplicationController
 
   def create
 
-    #Rails.logger.log params[:envelope][:to] # print the to field to the logs
-    Rails.logger.log params[:subject] # print the subject to the logs
-    Rails.logger.log params[:plain] # print the decoded body plain to the logs if present
-    #Rails.logger.log params[:html] # print the html decoded body to the logs if present
-    #Rails.logger.log params[:attachments][0] if params[:attachments] # A tempfile attachment if attachments is populated
+    Rails.logger.debug params.inspect
+    Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
 
   end
 end
