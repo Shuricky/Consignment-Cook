b653a5501581d23412d366690bf76c0510e59926
Shurick <alexr0909@yahoo.com>
2019-02-06 01:38:45 -0500

    25 maybe

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -15,9 +15,9 @@ class IncomingMailsController < ApplicationController
     Rails.logger.info params[:plain] #=> "Test with HTML."
     #Rails.logger.info params[:plain] #=> "<html><head>\r\n<meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\"></head><body\r\n bgcolor=\"#FFFFFF\" text=\"#000000\">\r\nTest with <span style=\"font-weight: bold;\">HTML</span>.<br>\r\n</body>\r\n</html>"
     style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(\d+)/).flatten
-    Rails.logger.info Style
-    Rails.logger.info Price
-    Rails.logger.info Stock
+    Rails.logger.info style
+    Rails.logger.info price
+    Rails.logger.info stock
 
   end
 end
