8241aec3da11d944384ee9d324a3bda2c4cfea68
Shurick <alexr0909@yahoo.com>
2019-02-06 04:11:58 -0500

    52

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -3,20 +3,23 @@ class IncomingMailsController < ApplicationController
 
   def create
 
-    Rails.logger.debug params.inspect
-    Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
-    Rails.logger.debug params[:plain]
+    #Rails.logger.debug params.inspect
+    #Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
+    #Rails.logger.debug params[:plain]
 
-    style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(\d+)/).flatten
+    style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(.+)/).flatten
     #style, price, stock = ary[0].sub(/Style /, ''), ary[1].sub(/Price \$/, ''), ary[2].sub(/Stock # /, '')
 
-    Rails.logger.debug style
-    Rails.logger.debug price
-    Rails.logger.debug stock
+    #Rails.logger.debug style
+    #Rails.logger.debug price
+    #Rails.logger.debug stock
 
     tokens = params[:plain].split
     spot = tokens.index("Quantity")
-    Rails.logger.debug tokens[spot+1]
+    size = tokens[spot+1]
+
+    shoe = Shoe.first(:conditions => ["sku = ? AND size = ? AND price = ?", style, size, price])
+    shoe.stockId = stock
 
   end
 end
