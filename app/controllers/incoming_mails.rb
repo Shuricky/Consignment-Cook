7c342f50d91aa5f8b6d1d503fa3367b73022de69
Shurick <alexr0909@yahoo.com>
2019-02-06 03:34:11 -0500

    50 something

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -8,6 +8,7 @@ class IncomingMailsController < ApplicationController
     Rails.logger.debug params[:plain]
 
     style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(\d+)/).flatten
+    #style, price, stock = ary[0].sub(/Style /, ''), ary[1].sub(/Price \$/, ''), ary[2].sub(/Stock # /, '')
 
     Rails.logger.debug style
     Rails.logger.debug price
@@ -15,7 +16,7 @@ class IncomingMailsController < ApplicationController
 
     tokens = params[:plain].split
     spot = tokens.index("Quantity")
-    Rails.logger.debug tokens[spot+1]
+    Rails.logger.debug tokens[spot+2]
 
   end
 end
