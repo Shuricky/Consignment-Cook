45c8ec1fc279b33f98f7dd37a43d09fe560ee85a
Shurick <alexr0909@yahoo.com>
2019-02-07 04:25:38 -0500

    90

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -7,7 +7,7 @@ class IncomingMailsController < ApplicationController
     #Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
     #Rails.logger.debug params[:plain]
 
-    if params[:headers][:subject] = "Fwd: Your shoes are listed!"
+    if params[:headers][:subject] == "Fwd: Your shoes are listed!"
 
       style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(.+)/).flatten
       style.chomp!
@@ -19,7 +19,7 @@ class IncomingMailsController < ApplicationController
       shoe = Shoe.where(:sku => style, :price => price.to_f, :size => sizeOther, :sold => "false").first
       shoe.update_column(:stockId, stock)
 
-    elseif params[:headers][:subject] = "Fwd: Your shoes have sold!"
+    elseif params[:headers][:subject] == "Fwd: Your shoes have sold!"
       stock = params[:plain].scan(/^(?:Stock \# )(.+)/).flatten
       stock.chomp!
       Rails.logger.debug stock
