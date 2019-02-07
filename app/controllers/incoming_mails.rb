3d3198df1c482963cfa9a95eb373a94cbebef4b6
Shurick <alexr0909@yahoo.com>
2019-02-07 04:46:20 -0500

    94

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -7,6 +7,7 @@ class IncomingMailsController < ApplicationController
     #Rails.logger.debug "Received: #{params[:headers][:subject]} for #{params[:envelope][:to]}"
     #Rails.logger.debug params[:plain]
     Rails.logger.debug params[:headers]['Subject'].chomp
+    Rails.logger.debug params[:headers]['Subject'].chomp == "Fwd: Your shoes are listed!"
     if params[:headers]['Subject'].chomp == "Fwd: Your shoes are listed!"
 
       style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(.+)/).flatten
