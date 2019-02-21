9ea41faa4cb9de3be4fbd2f8ed3e6f44f8f1352b
Shurick <alexr0909@yahoo.com>
2019-02-21 18:56:32 -0500

    173

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -10,7 +10,7 @@ class IncomingMailsController < ApplicationController
     #Rails.logger.debug params[:plain]
     Rails.logger.debug params[:headers]['Subject'].strip
     Rails.logger.debug params[:headers]['Subject'].strip == "Your shoes have sold!"
-    if params[:headers]['Subject'].strip == "Your shoes are listed!"
+    if (params[:headers]['Subject'].strip == "Your shoes are listed!") OR (params[:headers]['Subject'].strip == "Fwd: Your shoes are listed")
 
       #style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(.+)/).flatten
       #style.chomp!
