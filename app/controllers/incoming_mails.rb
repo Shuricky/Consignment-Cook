a95c3ccda5f0ae5f41cdbc6409cdf72a47633334
Shurick <alexr0909@yahoo.com>
2019-05-07 13:10:50 -0400

    1002

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -14,7 +14,7 @@ class IncomingMailsController < ApplicationController
     Rails.logger.debug params[:headers]['Subject'].strip
     Rails.logger.debug params[:headers]['Subject'].strip == "Your shoes have sold!"
 
-    if params[:headers]['Subject'].strip == "Fwd: Your shoes are listed!"
+    if params[:headers]['Subject'].strip == "Your shoes are listed!"
 
       #style, price, stock = params[:plain].scan(/^(?:Style |Price \$|Stock \# )(.+)/).flatten
       #style.chomp!
@@ -55,7 +55,7 @@ class IncomingMailsController < ApplicationController
         shoe.update_column(:stockId, stock)
       end
 =end
-    elsif params[:headers]['Subject'].strip == "Fwd: Your shoes have sold!"
+    elsif params[:headers]['Subject'].strip == "Your shoes have sold!"
       #stock = params[:plain].scan(/^(?:Stock \# )(.+)/).flatten
 
       #stock[0].chomp!
