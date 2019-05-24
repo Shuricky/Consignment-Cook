ef09953bccd5d2244d1e753d53c4f1355d063bcd
Shurick <alexr0909@yahoo.com>
2019-05-24 17:19:35 -0400

    1011

--- app/controllers/incoming_mails.rb
+++ app/controllers/incoming_mails.rb
@@ -85,8 +85,8 @@ class IncomingMailsController < ApplicationController
       shoe = Shoe.where(:stockId => stock).first
       if (shoe != nil)
         shoe.update_attribute(:sold, "true")
-        if shoe.price != price2.to_f
-          shoe.update_attribute(:price, price2.to_f)
+        if shoe.price != price2.tr!(',', '').to_f
+          shoe.update_attribute(:price, price2.tr!(',', '').to_f)
         end
         userToUpdate = shoe.user_id
         user2 = User.where(:id => userToUpdate).first
