458d3d5a32fc1c1d4054e7a8658dfbc8bd271a00
Shurick <alexr0909@yahoo.com>
2019-05-28 23:38:45 -0400

    1050

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -65,7 +65,7 @@ class ShoesController < ApplicationController
   def import
    if params[:file]
      Shoe.import(params[:file], current_user.id)
-     redirect_to "/shoes", notice: "Activity Data imported!"
+     redirect_to "/shoes"
    end
  end
 
